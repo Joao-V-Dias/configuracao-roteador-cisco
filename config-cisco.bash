582b08db46099a76daee862336e08c25ec430bd1
Joao Dias <100037973+Joao-V-Dias@users.noreply.github.com>
2025-11-25 20:39:20 -0300

    Add g0/0.30 configuration and write memory command
    Added configuration for interface g0/0.30 and write memory command.

--- config-cisco.bash
+++ config-cisco.bash
@@ -11,12 +11,13 @@ interface g0/0.20
  encapsulation dot1q 20
  ip address 192.168.20.1 255.255.255.0
 exit
-interface g0/0.99
- encapsulation dot1q 99
- ip address 192.168.99.1 255.255.255.0
+interface g0/0.30
+ encapsulation dot1q 30
+ ip address 192.168.30.1 255.255.255.0
 exit
 interface loopback 0
  ip address 8.8.8.8 255.255.255.255
 exit
 end
+
 write memory
